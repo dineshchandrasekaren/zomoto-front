3e26a93e2eea8051aab35e3b90c6219e8697480e
dinesh chandrasekaren <64305855+dineshchandrasekaren@users.noreply.github.com>
2024-04-26 12:47:55 +0530

    Update RestraurentCard.jsx

--- src/Components/RestraurentCard/RestraurentCard.jsx
+++ src/Components/RestraurentCard/RestraurentCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { TiStarFullOutline } from "react-icons/ti";
 import Carousel from "../Carousel";
 import Rating from "../Reusable/ratingColor";
-
+import { Link } from "react-router-dom";
 function RestraurentCard({
   name,
   isPro,
@@ -18,7 +18,7 @@ function RestraurentCard({
   const image2 =
     "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp";
   return (
-    <>
+    <Link to="/restraurant/123">
       <div className="flex flex-col cursor-pointer transition duration-700 customshadow lg:w-1/3 md:w-1/2 w-full c gap-1.5 p-2 rounded-2xl  hover:border-2 hover:border-blue-600 ">
         <div className="relative  rounded-xl">
           <img
@@ -156,7 +156,7 @@ function RestraurentCard({
           </div>
         </div>
       </div>
-    </>
+    </Link>
   );
 }
 
